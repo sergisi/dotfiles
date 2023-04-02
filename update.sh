263b9192a81f5982051ab58456436d26e41bc84a
sergisi <sergisiba@gmail.com>
2023-04-02 23:06:54 +0700

    More thingy

--- update.sh
+++ update.sh
@@ -9,7 +9,10 @@ pacman -Qe > packages.txt
 set config ~sergi/.config
 cp "$config/BetterDiscord/themes" config/BetterDiscord/ -r
 cp ~sergi/.doom.d/config.el ~sergi/.doom.d/custom.el ~sergi/.doom.d/init.el ~sergi/.doom.d/packages.el ~sergi/.doom.d/snippets ~sergi/.doom.d/themes doom -r
-for f in fish helix hypr kitty mako pipewire rofi spotify-tui swaylock wallpapers waybar wlogout;
+mkdir config/spotify-tui
+cp ~sergi/.config/spotify-tui/config.yml ~sergi/.config/spotify-tui/frappe.yml ~sergi/.config/spotify-tui/latte.yml ~sergi/.config/spotify-tui/macchiato.yml ~sergi/.config/spotify-tui/mocha.yml config/spotify-tui
+
+for f in fish helix hypr kitty mako pipewire rofi swaylock wallpapers waybar wlogout;
     cp "$config/$f" "config" -r
 end
 
